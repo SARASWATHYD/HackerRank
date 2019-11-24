a615894dab4f2a45637772b6e31b17cf68f6f480
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2019-11-24 19:21:29 +0530

    today challenge

--- src/WarnPrisoner.java
+++ src/WarnPrisoner.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class WarnPrisoner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i= 0 ; i < n ; i ++) {
+			
+			int prisoner = sc.nextInt();
+			int sweets = sc.nextInt();
+			int chair = sc.nextInt();
+			
+			}
 	}
 
 }
