8fe00c15f51b0646093e869a5a43ead2ffe4f2bb
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2019-12-17 19:55:53 +0530

    today hacker rank

---
+++ src/CircularRotation.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CircularRotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int elem[] = new int[n];
+ 		for(int i = 0; i < n ; i ++) {
+			 elem[i] = sc.nextInt();
+		}
+ 		circularRotation(elem);
+	}
+	
+	public static void circularRotation(int[] elements) {
+		
+		for(int i = 0 ; i < elements.length ; i ++) {
+			elements[i]
+		}
+	}
+
+}
