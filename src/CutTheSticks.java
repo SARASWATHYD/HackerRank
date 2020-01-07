d2bd70d4e0f6e2a5a7caf89bd0a50027803ac0a2
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-01-07 11:19:24 +0530

    cut stick commit

--- src/CutTheSticks.java
+++ src/CutTheSticks.java
@@ -1,9 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class CutTheSticks {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] sticks= new int[n];
+		for(int i = 0; i <n ; i++)
+			sticks[i]= sc.nextInt();
+	//solve it 		
+		Arrays.sort(sticks);
+		int sticksLeft = n;
+        
+        int curr = sticks[0];
+        int currCount = 0;
+        System.out.println(n);
+        
+        //Works by decrementing sticksLeft by the frequency of the smallest stick each time
+        for(int i = 0; i < n; i++)
+        {
+            if(curr == sticks[i])
+            {
+                currCount++;
+            }
+            else
+            {
+                sticksLeft -= currCount;
+                currCount = 1;
+                curr = sticks[i];
+                System.out.println(sticksLeft);
+            }
 
 	}
+	}
+	}
+	
+	
 
-}
