ab3df93d02358dfcb7f240ff74c34660a4fe82c7
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-01-25 15:26:41 +0530

    today commit

---
+++ src/TaumBday.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TaumBday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		for (int i = 0; i < number; i++) {
+			int b = sc.nextInt();
+			int w = sc.nextInt();
+			int bc = sc.nextInt();
+			int wc = sc.nextInt();
+			int z = sc.nextInt();
+		}
+
+
+	}
+
+}
