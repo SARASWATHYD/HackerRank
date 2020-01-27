d3267c4915e47c6f10db8632fd32283b25c74580
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-01-27 16:30:55 +0530

    done with today problem

---
+++ src/NonSubSet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NonSubSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = sc.nextInt();
+
+	}
+
+}
--- src/TaumBday.java
+++ src/TaumBday.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class TaumBday {
@@ -5,16 +6,23 @@ public class TaumBday {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		for (int i = 0; i < number; i++) {
-			int b = sc.nextInt();
-			int w = sc.nextInt();
-			int bc = sc.nextInt();
-			int wc = sc.nextInt();
-			int z = sc.nextInt();
+		int t=sc.nextInt();
+		for(int k=0;k<t;k++){
+		BigInteger b=new BigInteger(sc.next());
+		BigInteger w=new BigInteger(sc.next());
+		BigInteger x=new BigInteger(sc.next());
+		BigInteger y=new BigInteger(sc.next());
+		BigInteger z=new BigInteger(sc.next());
+		if(x.compareTo(y.add(z))==1)
+			System.out.println((b.multiply(y.add(z))).add(w.multiply(y)));
+		else if(y.compareTo(x.add(z))==1)
+			System.out.println(b.multiply(x).add(w.multiply(x.add(z))));
+		else System.out.println(b.multiply(x).add(w.multiply(y)));
+			}
 		}
-
-
-	}
-
 }
+
+
+	
+
+
