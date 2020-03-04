c982e1081d0e5f26aac8370cff595a75aa55681c
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-03-04 15:34:05 +0530

    today commit

---
+++ src/KarprekarNumber.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class KarprekarNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+		findKarprekar(start, end);
+	}
+
+	private static void findKarprekar(int start, int end) {
+		
+		for (int i = start; i <= end; i++) {
+			if (isKarper(i))
+				System.out.print(i + " ");
+		}
+		
+		}
+
+	private static boolean isKarper(int n) {
+		if (n == 1)
+			return true;
+		
+		int sq_n = n * n;
+		int count_digits = 0;
+		while (sq_n != 0) {
+			count_digits++;
+			sq_n /= 10;
+		}
+
+		sq_n = n * n; 
+
+		for (int r_digits = 1; r_digits < count_digits; r_digits++) {
+			int eq_parts = (int) Math.pow(10, r_digits);
+			if (eq_parts == n)
+				continue;
+
+			int sum = sq_n / eq_parts + sq_n % eq_parts;
+			if (sum == n)
+				return true;
+		}
+		return false;
+
+	}
+}
