dd9230985d290517148d5415feece9d063846d0b
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-05-14 17:14:22 +0530

    today challenge

--- src/leet/MedianTwoSortedArrays.java
+++ src/leet/MedianTwoSortedArrays.java
@@ -1,32 +1,26 @@
 package leet;
 
-import java.util.Scanner;
-
 public class MedianTwoSortedArrays {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int ar[] = new int[sc.nextInt()];
-		for (int i = 0; i < ar.length; i++) {
-			ar[i] = sc.nextInt();
-		}
-		int ar2[] = new int[sc.nextInt()];
-		for (int i = 0; i < ar2.length; i++) {
-			ar2[i] = sc.nextInt();
-		}
-		System.out.println(findMedianSortedArrays(ar, ar2));
-
-		System.out.println(findMedian(ar));
-
-	}
 
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		if (nums1.length <= 0)
-			return findMedian(nums2);
-		if (nums2.length <= 0)
-			return findMedian(nums1);
-		return (findMedian(nums1) + findMedian(nums2)) / 2;
+		int i = 0, j = 0, k = 0;
+		int n1 = nums1.length;
+		int n2 = nums2.length;
+		int[] merge = new int[n1 + n2];
+
+		while (i < n1 && j < n2) {
+			if (nums1[i] < nums2[j])
+				merge[k++] = nums1[i++];
+			else
+				merge[k++] = nums2[j++];
+		}
+		while (i < n1)
+			merge[k++] = nums1[i++];
+		while (j < n2)
+			merge[k++] = nums2[j++];
+
+		return findMedian(merge);
 	}
 
 	private  static double findMedian(int[] num){
@@ -34,5 +28,6 @@ public class MedianTwoSortedArrays {
       return (size % 2 == 0) ? ((Double.valueOf(num[size / 2]) + Double.valueOf(num[(size / 2) - 1])) / 2)
 				: Double.valueOf(num[(size / 2)]);
 
-
+	}
+	
 }
