0bd16d24583f68bb6a24aa05edc9969ca197c01b
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-05-27 13:39:05 +0530

    palindrome

---
+++ src/leet/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package leet;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int str1 = sc.nextInt();
+		String str = String.valueOf(str1);
+		char[] s_char = str.toCharArray();
+		boolean isPalindrome = true;
+		for (int x = 0, y = s_char.length - 1; x < s_char.length; x++, y--)
+			isPalindrome = s_char[x] == s_char[y] ? true : false;
+
+		System.out.println(isPalindrome);
+	}
+
+}
