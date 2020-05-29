b52ced41f9a319b697b3d3b4040d93188b923d45
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-05-29 16:28:42 +0530

    today

---
+++ src/leet/LongestPalindromicSubString.java
@@ -0,0 +1,20 @@
+package leet;
+
+import java.util.Scanner;
+
+public class LongestPalindromicSubString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(new LongestPalindromicSubString().longestPalindrome(sc.next()));
+		// TODO Auto-generated method stub
+
+	}
+
+	public String longestPalindrome(String s) {
+//		if (isPalindrome(s))
+			return s;
+//		else
+//			splitAndCheckPalindorme();
+	}
+}
