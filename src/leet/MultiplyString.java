e76f30b08d11a269357ea017ed4f92767f5334e1
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-06-06 21:35:24 +0530

    today challenge

---
+++ src/leet/MultiplyString.java
@@ -0,0 +1,18 @@
+package leet;
+
+import java.util.Scanner;
+
+public class MultiplyString {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int num1 = Integer.valueOf(sc.next());
+		int num2 = Integer.valueOf(sc.next());
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num1 * num2);
+		System.out.println(String.valueOf(num1 * num2));
+	}
+
+}
