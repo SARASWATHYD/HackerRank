fa8a6f5bc87d4694b22af597c4d5c3477e842a17
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-07-01 14:51:46 +0530

    added

--- src/leet/LongestCommonPrefix.java
+++ src/leet/LongestCommonPrefix.java
@@ -2,6 +2,20 @@ package leet;
 
 import java.util.Scanner;
 
+/**
+ * Write a function to find the longest common prefix string amongst an array of
+ * strings.
+ * 
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Example 1:
+ * 
+ * Input: ["flower","flow","flight"] Output: "fl"
+ * 
+ * @author saraswathy
+ *
+ */
+
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
