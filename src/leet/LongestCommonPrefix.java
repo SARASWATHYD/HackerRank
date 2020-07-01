e574eba72598c7c4c70b23b60dc6c9a081ad4552
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-07-01 14:50:00 +0530

    added new class

---
+++ src/leet/LongestCommonPrefix.java
@@ -0,0 +1,39 @@
+package leet;
+
+import java.util.Scanner;
+
+public class LongestCommonPrefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String[] strings = new String[sc.nextInt()];
+		for (int i = 0; i < strings.length; i++)
+			strings[i] = sc.next();
+
+		System.out.println(longestCommonPrefix(strings));
+		}
+
+	public static String longestCommonPrefix(String[] strs) {
+		if (strs.length == 0)
+			return "";
+		if (strs.length == 1)
+			return strs[0];
+		int i = 0;
+		char ch = '0';
+		while (true) {
+			for (String str : strs) {
+				if (i >= str.length())
+					return str;
+				if (ch == '0') {
+					ch = str.charAt(i);
+				}
+				if (str.charAt(i) != ch)
+					return str.substring(0, i);
+			}
+			i++;
+			ch = '0';
+		}
+	}
+
+}
