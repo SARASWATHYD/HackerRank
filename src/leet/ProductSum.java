ec8fb883ed5a28be99641f6dca7578cc2d0bc7a8
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-12-03 20:27:19 +0530

    addded

--- src/leet/ProductSum.java
+++ src/leet/ProductSum.java
@@ -2,6 +2,14 @@ package leet;
 
 import java.util.Scanner;
 
+/**
+ * Input: n = 234 Output: 15 Explanation: Product of digits = 2 * 3 * 4 = 24 Sum
+ * of digits = 2 + 3 + 4 = 9 Result = 24 - 9 = 15
+ * 
+ * @author saraswathy
+ *
+ */
+
 public class ProductSum {
 
 	public int subtractProductAndSum(int n) {
