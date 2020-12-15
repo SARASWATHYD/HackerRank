192be4ae34a2249a2fa3db198fd774631221bec3
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2020-12-15 18:49:17 +0530

    today

---
+++ src/prblms/BillDivision.java
@@ -0,0 +1,71 @@
+package prblms;
+
+import java.util.Scanner;
+
+public class BillDivision {
+	/**
+	 * wo friends Anna and Brian, are deciding how to split the bill at a dinner.
+	 * Each will only pay for the items they consume. Brian gets the check and
+	 * calculates Anna's portion. You must determine if his calculation is correct.
+	 * 
+	 * For example, assume the bill has the following prices: . Anna declines to eat
+	 * item which costs . If Brian calculates the bill correctly, Anna will pay . If
+	 * he includes the cost of , he will calculate . In the second case, he should
+	 * refund to Anna.
+	 * 
+	 * Function Description
+	 * 
+	 * Complete the bonAppetit function in the editor below. It should print Bon
+	 * Appetit if the bill is fairly split. Otherwise, it should print the integer
+	 * amount of money that Brian owes Anna.
+	 * 
+	 * bonAppetit has the following parameter(s):
+	 * 
+	 * bill: an array of integers representing the cost of each item ordered k: an
+	 * integer representing the zero-based index of the item Anna doesn't eat b: the
+	 * amount of money that Anna contributed to the bill Input Format
+	 * 
+	 * The first line contains two space-separated integers and , the number of
+	 * items ordered and the -based index of the item that Anna did not eat. The
+	 * second line contains space-separated integers where . The third line contains
+	 * an integer, , the amount of money that Brian charged Anna for her share of
+	 * the bill.
+	 * 
+	 * Constraints
+	 * 
+	 * The amount of money due Anna will always be an integer Output Format
+	 * 
+	 * If Brian did not overcharge Anna, print Bon Appetit on a new line; otherwise,
+	 * print the difference (i.e., ) that Brian must refund to Anna. This will
+	 * always be an integer.
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		/*
+		 * input 4 1 3 10 2 9 12
+		 */
+		// output need to be 5 because 12-7 = 5
+		Scanner sc = new Scanner(System.in);
+		int itemSize = sc.nextInt();
+		int briana_not_had = sc.nextInt();
+		int[] items = new int[itemSize];
+		for (int i = 0; i < itemSize; i++)
+			items[i] = sc.nextInt();
+		int brianaGot = sc.nextInt();
+		System.out.println(new BillDivision().checkBill(items, briana_not_had, brianaGot));
+
+	}
+
+	private String checkBill(int[] items, int exclude, int brianaGot) {
+		int sum = 0;
+		for (int i = 0; i < items.length; i++) {
+			if (i != exclude)
+				sum = sum + items[i];
+		}
+		return (sum / 2 == brianaGot) ? "Bon Appetit" : String.valueOf(brianaGot - sum / 2);
+
+	}
+}
